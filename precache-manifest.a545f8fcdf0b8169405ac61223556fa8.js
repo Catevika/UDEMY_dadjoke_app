e35c4420819c7216976b09eed09a769fb54423d5
Catevika <dominique.bello@outlook.fr>
2019-07-13 18:22:18 +0200

    Updates

---
+++ precache-manifest.a545f8fcdf0b8169405ac61223556fa8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c97971c5a9b987fa176a2442657fa0e2",
+    "url": "/UDEMY_dadjoke_app/index.html"
+  },
+  {
+    "revision": "e9eb4d21c1cbbe9dee6e",
+    "url": "/UDEMY_dadjoke_app/static/css/main.3d07f25e.chunk.css"
+  },
+  {
+    "revision": "686f72326526d985683c",
+    "url": "/UDEMY_dadjoke_app/static/js/2.199fd749.chunk.js"
+  },
+  {
+    "revision": "e9eb4d21c1cbbe9dee6e",
+    "url": "/UDEMY_dadjoke_app/static/js/main.909a38f5.chunk.js"
+  },
+  {
+    "revision": "6cb46fffc7ce7bdbd785",
+    "url": "/UDEMY_dadjoke_app/static/js/runtime~main.ffc40062.js"
+  }
+]);
