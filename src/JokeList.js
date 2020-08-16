32c2a3080934635ee65ce1bb0d5197864c3bd4f3
Catevika <dominique.bello@outlook.fr>
2020-08-16 14:54:07 +0200

    npm packages update

--- src/JokeList.js
+++ src/JokeList.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
 import Joke from './Joke';
 import axios from 'axios';
-import uuid from 'uuid/v4';
+import { v4 as uuidv4 } from 'uuid';
 import './JokeList.css';
 
-class JokeList extends Component {
+class JokeList extends Component
+{
 	static defaultProps = {
 		numJokesToGet: 10
 	};
-	constructor(props) {
+	constructor(props)
+	{
 		super(props);
 		this.state = {
 			jokes: JSON.parse(window.localStorage.getItem('jokes') || '[]'),
@@ -18,10 +20,12 @@ class JokeList extends Component {
 		console.log(this.seenJokes);
 		this.handleClick = this.handleClick.bind(this);
 	}
-	componentDidMount() {
+	componentDidMount()
+	{
 		if (this.state.jokes.length === 0) this.getJokes();
 	}
-	async getJokes() {
+	async getJokes()
+	{
 		try {
 			let jokes = [];
 			while (jokes.length < this.props.numJokesToGet) {
@@ -30,7 +34,7 @@ class JokeList extends Component {
 				});
 				let newJoke = res.data.joke;
 				if (!this.seenJokes.has(newJoke)) {
-					jokes.push({ id: uuid(), text: newJoke, votes: 0 });
+					jokes.push({ id: uuidv4(), text: newJoke, votes: 0 });
 				} else {
 					console.log('FOUND A DUPLICATE!');
 					console.log(newJoke);
@@ -49,7 +53,8 @@ class JokeList extends Component {
 			this.setState({ loading: false });
 		}
 	}
-	handleVote(id, delta) {
+	handleVote(id, delta)
+	{
 		this.setState(
 			st => ({
 				jokes: st.jokes.map(j =>
@@ -60,10 +65,12 @@ class JokeList extends Component {
 				window.localStorage.setItem('jokes', JSON.stringify(this.state.jokes))
 		);
 	}
-	handleClick() {
+	handleClick()
+	{
 		this.setState({ loading: true }, this.getJokes);
 	}
-	render() {
+	render()
+	{
 		if (this.state.loading) {
 			return (
 				<div className='JokeList-spinner'>
